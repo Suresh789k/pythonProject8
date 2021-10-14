737823b9d3413bd0630f2334bfab65a06881bcc7
Suresh <surescharyk94@gmail.com>
2021-10-14 23:38:06 +0530

    Another one added

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+from secret import password
 
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+def my_function(a,b):
+    return a+b
 
 
 # Press the green button in the gutter to run the script.
@@ -14,5 +14,7 @@ if __name__ == '__main__':
     print_hi('PyCharm')
     print("hello world")
     print("where do you live?")
+    print('password')
 
 
+    print(my_function(3,10))
